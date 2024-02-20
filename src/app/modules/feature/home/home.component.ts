f743b410256c243391d54e0a818b0d25624be2a1
mandeepbartwal <bartwalmandeep99@gmail.com>
2024-02-20 23:25:22 +0530

    implement search

--- src/app/modules/feature/home/home.component.ts
+++ src/app/modules/feature/home/home.component.ts
@@ -2,26 +2,42 @@ import { Component, OnInit } from '@angular/core'
 import { SharedService } from '../../../shared/services/shared.service'
 import { productData } from '../../../shared/types/shared.types'
 import { CurrencyPipe } from '@angular/common'
-import { BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs'
 import { ButtonComponent } from '../../../shared/components/UI/button/button.component'
 import { RouterModule } from '@angular/router'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CurrencyPipe, ButtonComponent, RouterModule],
+  imports: [CurrencyPipe, ButtonComponent, RouterModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   public productData: productData | null = null
-  public searchText: string = ''
+  searchInput: FormControl;
   searchQuery$ = new BehaviorSubject<string>('')
   name: string = 'Add to cart'
-  constructor(private _sharedServices: SharedService) {}
+  private _unsubscribeAll: any
+  constructor(private _sharedServices: SharedService) {
+    this.searchInput = new FormControl('');
+    this._unsubscribeAll = new Subject();
+  }
 
   ngOnInit(): void {
     this.fetchAllProducts()
+    this.searchInput.valueChanges
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        debounceTime(300),
+        distinctUntilChanged(),
+      )
+      .subscribe((searchText) => {
+        this._sharedServices.searchProduct(searchText).subscribe((res: any) => {
+          this.productData = res;
+        });
+      });
   }
 
   searchProduct(searchValue: string) {
--- src/app/modules/feature/products/products.component.ts
+++ src/app/modules/feature/products/products.component.ts
@@ -1,36 +1,43 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { product, productData } from './../../../shared/types/shared.types'
-import { Component, OnInit } from '@angular/core'
-import { SharedService } from '../../../shared/services/shared.service'
-import { CurrencyPipe, PercentPipe } from '@angular/common'
-import { ButtonComponent } from '../../../shared/components/UI/button/button.component'
-import { ModalComponent } from '../../../shared/components/UI/modal/modal.component'
-import { RouterModule } from '@angular/router'
-import { FormControl, ReactiveFormsModule } from '@angular/forms'
-import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs'
+import { product, productData } from './../../../shared/types/shared.types';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../../shared/services/shared.service';
+import { CurrencyPipe, PercentPipe } from '@angular/common';
+import { ButtonComponent } from '../../../shared/components/UI/button/button.component';
+import { ModalComponent } from '../../../shared/components/UI/modal/modal.component';
+import { RouterModule } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Subject, debounceTime, distinctUntilChanged, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CurrencyPipe, PercentPipe, ButtonComponent, ModalComponent, RouterModule, ReactiveFormsModule],
+  imports: [
+    CurrencyPipe,
+    PercentPipe,
+    ButtonComponent,
+    ModalComponent,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
-  private _unsubscribeAll: Subject<any>
+  private _unsubscribeAll: Subject<any>;
 
-  productList: product[] = []
-  totalProduct: number = 0
-  currentPage: number = 1
-  searchInput: FormControl
+  productList: product[] = [];
+  totalProduct: number = 0;
+  currentPage: number = 1;
+  searchInput: FormControl;
 
   public modalBoxInfo: {
-    isVisible: boolean
-    id: number
-    content: string
-    title: string
-    modalType: string
-    buttonType: string
+    isVisible: boolean;
+    id: number;
+    content: string;
+    title: string;
+    modalType: string;
+    buttonType: string;
   } = {
     isVisible: false,
     id: 1,
@@ -38,26 +45,32 @@ export class ProductsComponent implements OnInit {
     title: '',
     modalType: '',
     buttonType: '',
-  }
+  };
   constructor(private _sharedService: SharedService) {
-    this.searchInput = new FormControl('')
-    this._unsubscribeAll = new Subject()
+    this.searchInput = new FormControl('');
+    this._unsubscribeAll = new Subject();
   }
 
   ngOnInit(): void {
-    this.fetchProducts()
+    this.fetchProducts();
     this.searchInput.valueChanges
-      .pipe(takeUntil(this._unsubscribeAll), debounceTime(300), distinctUntilChanged())
+      .pipe(
+        takeUntil(this._unsubscribeAll),
+        debounceTime(300),
+        distinctUntilChanged(),
+      )
       .subscribe((searchText) => {
-        console.log(searchText)
-      })
+        this._sharedService.searchProduct(searchText).subscribe((res: any) => {
+          this.productList = res.products;
+        });
+      });
   }
 
   fetchProducts = () => {
     this._sharedService.getAllProducts().subscribe((res: productData) => {
-      this.productList = res.products
-    })
-  }
+      this.productList = res.products;
+    });
+  };
 
   deleteProduct(id: number) {
     this.modalBoxInfo = {
@@ -67,16 +80,18 @@ export class ProductsComponent implements OnInit {
       title: 'Delete Product',
       modalType: 'delete',
       buttonType: 'Delete',
-    }
+    };
   }
   isModalOpen = (event: boolean) => {
-    this.modalBoxInfo.isVisible = event
-  }
+    this.modalBoxInfo.isVisible = event;
+  };
 
   sortProducts = (deletedProduct: number) => {
-    const index: number = this.productList.findIndex((item) => item.id === deletedProduct)
+    const index: number = this.productList.findIndex(
+      (item) => item.id === deletedProduct,
+    );
     if (index !== -1) {
-      this.productList.splice(index, 1)
+      this.productList.splice(index, 1);
     }
-  }
+  };
 }
