6e17873d0c2f99413aaff65dde9200e0794fe3c7
mandeepbartwal <bartwalmandeep99@gmail.com>
2024-02-25 23:39:32 +0530

    fix the authguard issue

--- src/app/modules/feature/learn/learn.component.ts
+++ src/app/modules/feature/learn/learn.component.ts
@@ -1,9 +1,10 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-learn',
   standalone: true,
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './learn.component.html',
   styleUrl: './learn.component.css',
 })
--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, Route, UrlSegment, UrlTree, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { SharedService } from '../services/shared.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +12,33 @@ export class AuthGuard implements CanActivate {
     private router: Router,
     private _sharedService: SharedService,
   ) {}
+  private _check(redirectURL: string): Observable<boolean> {
+    // Check the authentication status
+    return this._sharedService.check()
+      .pipe(
+        switchMap((authenticated) => {
 
-  canActivate(): boolean {
-    if (this._sharedService.authenticated) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+          if (!authenticated) {
+            // Redirect to the sign-in page
+            this.router.navigate(['/login']);
+
+            // Prevent the access
+            return of(false);
+          }
+
+          // Allow the access
+          return of(true);
+        })
+      );
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this._check('/login');
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const redirectUrl = state.url === '/login' ? '' : state.url;
+      return this._check(redirectUrl);
   }
 }
